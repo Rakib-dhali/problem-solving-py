4d892f19ac4c40bf81eb7208cdf2a5c977593f41
Rakib Dhali <rakibdhali343@gmail.com>
2025-06-12 12:02:50 +0600

    Add files via upload

---
+++ prime_number.py
@@ -0,0 +1,14 @@
+# Rakib Dhali
+# 
+
+number = int(input("Enter the number: "))
+
+if number <= 1:
+    print("No, the number isn't a Prime number.")
+else:
+    for i in range(2, number):
+        if number % i == 0:
+            print("No, the number isn't a Prime number.")
+            break
+    else:
+        print("Yes, the number is a Prime number.")
